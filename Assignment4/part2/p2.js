7c01939008aadfeaa3b81eab9048a20fdaa5c5e3
ayomideakerele168-boop <ayomideakerele168@gmail.com>
2026-04-09 01:28:39 -0400

    third commit after creation of style.css and p2.js

---
+++ Assignment4/part2/p2.js
@@ -0,0 +1,10 @@
+//<!--
+       // Name: Ayomide Akerele
+       //file: index.html
+       //date: March 20,2026
+       // -->//
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
