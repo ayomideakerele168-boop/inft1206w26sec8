b6bdf3945434aed05df0d5ae291255cf62b4626c
ayomideakerele168-boop <ayomideakerele168@gmail.com>
2026-04-09 00:07:55 -0400

    all class example commit

---
+++ week10/p10.js
@@ -0,0 +1,7 @@
+console.log("week 10");
+
+document.querySelector('#d1'),addEventListener('mouseover', function(){
+    this.StylePropertyMap.color="green";
+});
+
+document.querySelector('#id1').addEventListener(-)
