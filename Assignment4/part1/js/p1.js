af54381bc9cbaf5fbb5c085c32f4d9a5abe57afe
ayomideakerele168-boop <ayomideakerele168@gmail.com>
2026-04-09 00:35:24 -0400

    the let commit(4 commit)part 1

--- Assignment4/part1/js/p1.js
+++ Assignment4/part1/js/p1.js
@@ -10,18 +10,23 @@ function randomValueFromArray(array) {
 }
 
 // Raw text strings
+let characters = [
+  "Willy the Goblin",
+  "Big Daddy",
+  "Father Christmas"
+];
 
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
+let places = [
+  "the soup kitchen",
+  "Disneyland",
+  "the White House"
+];
 
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
+let events = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and slithered away"
+];
 
 // Partial return random string function
 
