c39f7554d8caa36d4435b4ce27801d4496c191a2
ayomideakerele168-boop <ayomideakerele168@gmail.com>
2026-04-10 02:49:18 -0400

    commit for completion of part 3(assignment 4)

--- Assignment4/part3/p3.js
+++ Assignment4/part3/p3.js
@@ -28,37 +28,51 @@ class Ball {
     this.color = color;
     this.size = size;
   }
+
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
+  }
 
-    class Ball {
-  // …
   update() {
     if (this.x + this.size >= width) {
-      this.velX = -this.velX;
+      this.velX = -Math.abs(this.velX);
     }
 
     if (this.x - this.size <= 0) {
-      this.velX = -this.velX;
+      this.velX = Math.abs(this.velX);
     }
 
     if (this.y + this.size >= height) {
-      this.velY = -this.velY;
+      this.velY = -Math.abs(this.velY);
     }
 
     if (this.y - this.size <= 0) {
-      this.velY = -this.velY;
+      this.velY = Math.abs(this.velY);
     }
 
     this.x += this.velX;
     this.y += this.velY;
   }
-}
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball)) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
   }
 }
+
+  
 
 const balls = [];
 
@@ -85,6 +99,19 @@ function loop() {
   for (const ball of balls) {
     ball.draw();
     ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
