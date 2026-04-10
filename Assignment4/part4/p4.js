825180b93924a91fa1a1e490e1fc873cb9f655d4
ayomideakerele168-boop <ayomideakerele168@gmail.com>
2026-04-10 04:14:27 -0400

    update on p4.js

--- Assignment4/part4/p4.js
+++ Assignment4/part4/p4.js
@@ -1,5 +1,6 @@
 // set up canvas
-
+const para = document.querySelector("p");
+let count = 0;
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -154,3 +155,46 @@ class EvilCircle extends Shape {
 
 }
 
+// define array to store balls and populate it
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+  balls.push(ball);
+  count++;
+  para.textContent = 'Ball count: ' + count;
+}
+
+const evilBall = new EvilCircle(random(0, width), random(0, height));
+
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    if (ball.exists) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
+  }
+
+  evilBall.draw();
+  evilBall.checkBounds();
+  evilBall.collisionDetect();
+
+  requestAnimationFrame(loop);
+}
+
+loop();
